31c5f30dd9e32b5f1dc75e964f63a0f79d3a38b9
Shankavi Thiruchchelvarajan <33494702+shankavi@users.noreply.github.com>
2021-12-01 02:47:41 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,10 +3,10 @@ public class Welcome {
  public static void main(String[] args) {
   // TODO Auto-generated method stub
     System.out.println("Hello Shankavi !!!");
-    
-	System.out.println("Welcome To HCL...");
+    System.out.println("How are you);
+//System.out.println("Welcome To HCL...");
 	 
-System.out.println("Welcome To HCL...");
+//System.out.println("Welcome To HCL...");
  }
 
 }
