61be9cf717a28a0a5df5535968ae04044773ad38
Shankavi Thiruchchelvarajan <33494702+shankavi@users.noreply.github.com>
2021-12-08 01:57:39 +0530

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -2,8 +2,8 @@ public class JenkinsExample {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello Shankavi");
-	System.out.println("How Are You !!!");
+System.out.println("Hello Shankavi");
+System.out.println("How Are You !!!");
 System.out.println("How Are You !!!");
 
  }
